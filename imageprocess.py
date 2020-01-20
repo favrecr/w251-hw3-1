d479ecd5f798cb3ed581e600cfabf09ccb388f87
root <root@JDS100.jds.com>
2020-01-20 06:51:13 +0000

    updates to image processing

--- imageprocess.py
+++ imageprocess.py
@@ -5,7 +5,7 @@ import paho.mqtt.client as mqtt
 #Set up MQTT parameters
 LOCAL_MQTT_HOST="hw3_broker"
 LOCAL_MQTT_PORT=1883
-LOCAL_MQTT_TOPIC="JDS/WEBCAMS/#"
+LOCAL_MQTT_TOPIC="JDS/WEBCAMS"
 
 #Path to store images
 OUTPUT_STR="/HW3Images/image"
@@ -15,30 +15,26 @@ def on_connect_local(client, userdata, flags, rc):
         print("connected to local broker with rc: " + str(rc))
         client.subscribe(LOCAL_MQTT_TOPIC,1) 
 
+index=0
 #Process incoming messages
 def on_message(client,userdata, msg):                                        
-  global index 
-  try:                                                                       
-    print("message received from"+client)                                               
-    msg = msg.payload                                                        
-    #print(msg)
-    frame=np.frombuffer(msg,np.int8)
-    #print("frame=",frame)
-    img=cv2.imdecode(frame,1)
-    #print("img - ",img)
-    cv2.imwrite(OUTPUT_STR+str(index)+".png",img)
-    index=index+1
+        global index 
+  	try:                                                                       
+    		print("message received!")                                               
+    		index=index+1
+                msgcontent = msg.payload                                                        
+                frame=np.frombuffer(msgcontent,np.int8)
+                img=cv2.imdecode(frame,1)
+                cv2.imwrite(OUTPUT_STR+str(index)+".png",img)
+        except:
+    	        print("Unexpected error:", sys.exc_info()[0])
 
-  except:
-    print("Unexpected error:", sys.exc_info()[0])
-
-index=0
 
 
 #Instantiate MQTT client
 local_mqttclient=mqtt.Client()
 local_mqttclient.on_connect = on_connect_local 
-local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)               
+local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 360)               
 local_mqttclient.on_message = on_message
 
 
